486f8b8eb9296fc4e06b0153d6f989b454412dec
guilletDev <guilletdev@gmail.com>
2023-03-29 21:47:13 -0300

    cambie el gap y los margin

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -5,10 +5,10 @@ import EventosCard from '../components/EventosCard'
 
 const Eventos = () => {
   return (
-    <div className={` ${styles.flexCenter} flex-col relative `}>
+    <div className={` ${styles.flexCenter} flex-col relative sm:mt-4`}>
       <br />
         <h1 className={`${styles.heading2} sm:mb-5 text-xl`}>EVENTOS REALIZADOS</h1>
-        <div className='py-8 mb-6 pb-4  grid sm:grid-cols-3 sm:grid-rows-2 sm:gap-6'>
+        <div className='py-8 mb-6 pb-4  grid sm:grid-cols-3 sm:grid-rows-2 sm:gap-x-8 '>
             {evento.map((card) => <EventosCard Key={card.id} {...card} />)}
         </div>
       <br /><br />
